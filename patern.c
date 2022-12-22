4b90c259da6191363d06ad7882eb1895057de33d
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-22 17:28:14 +0300

    Kalyoncugüller
    @themanoftalent

--- patern.c
+++ patern.c
@@ -1,17 +1,34 @@
-//melisa 
+#include<stdio.h>
 
+int main()
+{
+    int i,j,k,l;
 
-#include <stdio.h>
-#include <stdlib.h>
-#define kenar 8
+    printf("Enter the number of lines:");
+    scanf("%d",&k);
 
-int main() {
-    int i, j;
-    for (i = 1; i <= kenar; ++i) {
-        for (j = 1; j <= i; ++j) {
+    for(i=k; i>=1; --i)
+    {
+        for(l=0; l < k-i; ++l)
+            printf("  ");
+
+        for(j=i; j <= 2*i-1; ++j)
             printf("* ");
-        }
+
+        for(j=0; j < i-1; ++j)
+            printf("* ");
+
         printf("\n");
     }
+
     return 0;
 }
+
+/*output:
+Enter the number of lines:5
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        *       */
+        
