11f6757892a1087aaaa4c0a275ef5bfe15ea8759
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-22 18:10:44 +0300

    Hatice Serin
    @themanoftalent

--- union.c
+++ union.c
@@ -1,35 +1,34 @@
-// Melisa union struct birlikte
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-union information {
-    int age;
-    float gelir;
-    char name[20];
+//Lütfen geliştirip yazınız.
+#include"stdio.h"
+#include"stdlib.h"
+#include"string.h"
+union student{
+	int numara;
+	float sinavnotu;
+	char isim[20];	
 };
-
-struct student {
-    union information info;
-    enum {age, gelir, name} ozellik;
-};
-
-int main() {
-    struct student ogrenci;
-    ogrenci.ozellik = age;
-    ogrenci.info.age = 24;
-    printf("Age %d\n", ogrenci.info.age);
-    
-    ogrenci.ozellik = gelir;
-    ogrenci.info.gelir = 7000;
-    printf("Money %2.f\n", ogrenci.info.gelir);
-    
-    ogrenci.ozellik = name;
-    strcpy(ogrenci.info.name, "melisa");
-    printf("Name %s", ogrenci.info.name);
-    
-    
-    return 0;
+int main(){
+	int i;
+	int sinif=50;
+	union student veri;
+	basla:
+    for(i=0;i<sinif;i++){
+	printf("%d. student name:",i+1);
+	scanf("%s",veri.isim);
+	printf("%d. student number: ",i+1);
+	scanf("%d",&veri.numara);
+	printf("%d. student exam grade: ",i+1);
+	scanf("%f",&veri.sinavnotu);
+	if(veri.sinavnotu<50){
+		printf("%d. student exam grade %.2f .You failed the exam.\n",i+1,veri.sinavnotu);
+	}
+    else if(veri.sinavnotu>100){
+		printf("Gecersiz not girdiniz.\n");
+	}
+	else{
+		printf("%d. student exam grade %.2f .You passed the exam.\n",i+1,veri.sinavnotu);
+	}
+   }
+	goto basla;
+	return 0;
 }
