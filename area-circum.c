d193a23c3bdde0ab5595c5566818b04ec8ebeaa6
MelisaOnal <119419249+MelisaOnal@users.noreply.github.com>
2022-12-22 15:37:04 +0300

    melisa

--- area-circum.c
+++ area-circum.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-#include <conio.h>
-#define pi 3.14
 
-int main(){
-  
-    float r,circum,area;
-    
-    printf("Enter the radius of the circle:");
-    scanf("%f", &r);
-    
-    circum=2*pi*r;
-    area=pi*r*r;
-    
-    printf ("Area of the circle: %.2f"\n,area);
-    printf("Circumference of the circle: %.2f",circum);
-           
-    return 0;
+
+void alan(int a, int b) {
+    printf("Dikdörtgenin alanı %d\n", a * b);
 }
 
-//output:
-//Enter the radius of the circle:4
-//Area of the circle: 50.24
-//Circumference of the circle: 25.12
+void cevre(int a, int b) {
+    printf("Dikdörtgenin çevresi %d\n", 2 * (a + b));
+}
+
+int main() {
+    int a, b;
+    printf("Dikdörtgenin uzunluğunu girin: ");
+    scanf("%d", &a);
+    printf("Dikdörtgenin genişliğini girin: ");
+    scanf("%d", &b);
+    alan(a, b);
+    cevre(a, b);
+    return 0;
+}
